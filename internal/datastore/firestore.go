7f7df06397c2364993002c0bbbf746cbd235f5e6
Silvatek <steve@silvatek.uk>
2024-09-27 19:43:02 +0100

    Try connecting to Firestore

--- internal/datastore/firestore.go
+++ internal/datastore/firestore.go
@@ -1,18 +1,27 @@
 package datastore
 
 import (
+	"context"
 	"log"
 	"math/big"
+	"os"
 
+	"cloud.google.com/go/firestore"
 	"silvatek.uk/trustedassertions/internal/assertions"
 )
 
 type FireStore struct {
+	projectId    string
+	databaseName string
 }
 
 func InitFireStore() {
-	log.Print("Initialising FireStore")
-	ActiveDataStore = &FireStore{}
+	datastore := &FireStore{
+		projectId:    os.Getenv("GCLOUD_PROJECT"),
+		databaseName: os.Getenv("trustedassertions"),
+	}
+	log.Printf("Initialising FireStore: %s / %s", datastore.projectId, datastore.databaseName)
+	ActiveDataStore = datastore
 }
 
 func (fs *FireStore) Name() string {
@@ -26,6 +35,10 @@ func (fs *FireStore) FetchStatement(key string) assertions.Statement {
 }
 
 func (fs *FireStore) FetchEntity(key string) assertions.Entity {
+	projectID := os.Getenv("GCLOUD_PROJECT")
+	database := os.Getenv("trustedassertions")
+	client, _ := firestore.NewClientWithDatabase(context.Background(), projectID, database)
+	log.Print(client)
 	return assertions.NewEntity("{empty}", *big.NewInt(0))
 }
 
