802911fd019f439452598cd881e49cfa2b61aff0
Silvatek <steve@silvatek.uk>
2024-10-27 17:08:54 +0000

    Move webtest into its own internal package

--- cmd/server/web_test.go
+++ cmd/server/web_test.go
@@ -1,73 +1,114 @@
 package main
 
 import (
+	"crypto/rand"
+	"crypto/rsa"
+	"math/big"
+	"net/http"
+	"net/http/cookiejar"
+	"net/http/httptest"
 	"net/url"
 	"strings"
 	"testing"
 
 	"silvatek.uk/trustedassertions/internal/assertions"
+	"silvatek.uk/trustedassertions/internal/auth"
 	"silvatek.uk/trustedassertions/internal/datastore"
+	"silvatek.uk/trustedassertions/internal/web"
+	"silvatek.uk/trustedassertions/internal/webtest"
 )
 
+func setup(t *testing.T) *webtest.WebTest {
+	initLogging()
+
+	web.TemplateDir = "../../web"
+	testDataDir = "../../testdata"
+	initDataStore()
+
+	privateKey, _ := rsa.GenerateKey(rand.Reader, 2048)
+	signer := assertions.NewEntity("Signing entity", *big.NewInt(123456))
+	signer.MakeCertificate(privateKey)
+	datastore.ActiveDataStore.Store(&signer)
+	datastore.ActiveDataStore.StoreKey(signer.Uri(), assertions.PrivateKeyToString(privateKey))
+	web.DefaultEntityUri = signer.Uri()
+
+	wt := webtest.WebTest{}
+
+	jar, _ := cookiejar.New(nil)
+	wt.Client = &http.Client{
+		Jar: jar,
+	}
+
+	wt.User = &auth.User{Id: "admin"}
+	wt.Passwd = "testing"
+	wt.User.HashPassword(wt.Passwd)
+	wt.User.KeyRefs = append(wt.User.KeyRefs, auth.KeyRef{UserId: wt.User.Id, KeyId: signer.Uri().Unadorned(), Summary: ""})
+	datastore.ActiveDataStore.StoreUser(*wt.User)
+
+	wt.Server = httptest.NewServer(setupHandlers())
+
+	return &wt
+}
+
 func TestHomePage(t *testing.T) {
-	wt := NewWebTest(t)
+	wt := setup(t)
 	defer wt.Close()
 
-	page := wt.getPage("/")
-	page.assertHtmlQuery("h1", "Trusted Assertions")
+	page := wt.GetPage("/")
+	page.AssertHtmlQuery("h1", "Trusted Assertions")
 }
 
 func TestErrorPage(t *testing.T) {
-	wt := NewWebTest(t)
+	wt := setup(t)
 	defer wt.Close()
 
-	page := wt.getPage("/web/broken")
-	page.assertHtmlQuery("#intro", "Sorry, an error has occurred.")
-	page.assertHtmlQuery("#message", "Fake error for testing")
+	page := wt.GetPage("/web/broken")
+	page.AssertHtmlQuery("#intro", "Sorry, an error has occurred.")
+	page.AssertHtmlQuery("#message", "Fake error for testing")
 }
 
 func TestStatementPage(t *testing.T) {
-	wt := NewWebTest(t)
+	wt := setup(t)
 	defer wt.Close()
 
-	page := wt.getPage("/web/statements/e88688ef18e5c82bb8ea474eceeac8c6eb81d20ec8d903750753d3137865d10f")
-	page.assertHtmlQuery("#content", "The universe exists")
+	page := wt.GetPage("/web/statements/e88688ef18e5c82bb8ea474eceeac8c6eb81d20ec8d903750753d3137865d10f")
+	page.AssertHtmlQuery("#content", "The universe exists")
 }
 
 func TestEntityPage(t *testing.T) {
-	wt := NewWebTest(t)
+	wt := setup(t)
 	defer wt.Close()
 
-	page := wt.getPage("/web/entities/177ed36580cf1ed395e1d0d3a7709993ac1599ee844dc4cf5b9573a1265df2db")
-	page.assertHtmlQuery("#common_name", "Mr Tester")
+	page := wt.GetPage("/web/entities/177ed36580cf1ed395e1d0d3a7709993ac1599ee844dc4cf5b9573a1265df2db")
+	page.AssertHtmlQuery("#common_name", "Mr Tester")
 }
 
 func TestAssertionPage(t *testing.T) {
-	wt := NewWebTest(t)
+	wt := setup(t)
 	defer wt.Close()
 
-	page := wt.getPage("/web/assertions/514518bb09d57524bc6b96842721e4c4404cb4a3329aadf1761bb3eddb2832da")
-	page.assertHtmlQuery("#category", "IsTrue")
+	page := wt.GetPage("/web/assertions/514518bb09d57524bc6b96842721e4c4404cb4a3329aadf1761bb3eddb2832da")
+	page.AssertHtmlQuery("#category", "IsTrue")
 }
 
 func TestNewStatementPage(t *testing.T) {
-	wt := NewWebTest(t)
+	wt := setup(t)
 	defer wt.Close()
 
-	page := wt.getPage("/web/newstatement")
-	page.assertHtmlQuery("h2", "New Statement")
+	page := wt.GetPage("/web/newstatement")
+	page.AssertHtmlQuery("h2", "New Statement")
 }
 
 func TestPostNewStatement(t *testing.T) {
-	wt := NewWebTest(t)
+	wt := setup(t)
 	defer wt.Close()
 
 	data := url.Values{
 		"statement": {"Test statement"},
-		"sign_as":   {wt.user.KeyRefs[0].KeyId},
+		"sign_as":   {wt.User.KeyRefs[0].KeyId},
 	}
-	page := wt.postFormData("/web/newstatement", data)
-	page.assertSuccessResponse()
+	page := wt.PostFormData("/web/newstatement", data)
+	page.AssertSuccessResponse()
 
 	newUri := assertions.UriFromString(strings.TrimSpace(page.Find("#uri")))
 
@@ -79,15 +120,15 @@ func TestPostNewStatement(t *testing.T) {
 }
 
 func TestNewEntity(t *testing.T) {
-	wt := NewWebTest(t)
+	wt := setup(t)
 	defer wt.Close()
 
-	page := wt.getPage("/web/newentity")
-	page.assertSuccessResponse()
-	page.assertHtmlQuery("label", "Entity name")
+	page := wt.GetPage("/web/newentity")
+	page.AssertSuccessResponse()
+	page.AssertHtmlQuery("label", "Entity name")
 
-	page = wt.postFormData("/web/newentity", url.Values{"commonname": {"Test entity"}})
-	page.assertSuccessResponse()
+	page = wt.PostFormData("/web/newentity", url.Values{"commonname": {"Test entity"}})
+	page.AssertSuccessResponse()
 	uri := assertions.UriFromString(page.Find("span.fulluri"))
 
 	newEntity, err := datastore.ActiveDataStore.FetchEntity(uri)
@@ -100,23 +141,23 @@ func TestNewEntity(t *testing.T) {
 }
 
 func TestAddAssertion(t *testing.T) {
-	wt := NewWebTest(t)
+	wt := setup(t)
 	defer wt.Close()
 
-	page := wt.getPage("/web/statements/e88688ef18e5c82bb8ea474eceeac8c6eb81d20ec8d903750753d3137865d10f")
-	page.assertHtmlQuery("a", "Add a new assertion for this statement.")
+	page := wt.GetPage("/web/statements/e88688ef18e5c82bb8ea474eceeac8c6eb81d20ec8d903750753d3137865d10f")
+	page.AssertHtmlQuery("a", "Add a new assertion for this statement.")
 
-	page = wt.getPage("/web/statements/e88688ef18e5c82bb8ea474eceeac8c6eb81d20ec8d903750753d3137865d10f/addassertion")
-	page.assertSuccessResponse()
-	page.assertHtmlQuery("label", "Assertion:")
+	page = wt.GetPage("/web/statements/e88688ef18e5c82bb8ea474eceeac8c6eb81d20ec8d903750753d3137865d10f/addassertion")
+	page.AssertSuccessResponse()
+	page.AssertHtmlQuery("label", "Assertion:")
 
 	values := url.Values{
 		"assertion_type": {"IsTrue"},
 		"confidence":     {"0.75"},
-		"sign_as":        {wt.user.KeyRefs[0].KeyId},
+		"sign_as":        {wt.User.KeyRefs[0].KeyId},
 	}
-	page = wt.postFormData("/web/statements/e88688ef18e5c82bb8ea474eceeac8c6eb81d20ec8d903750753d3137865d10f/addassertion", values)
-	page.assertSuccessResponse()
+	page = wt.PostFormData("/web/statements/e88688ef18e5c82bb8ea474eceeac8c6eb81d20ec8d903750753d3137865d10f/addassertion", values)
+	page.AssertSuccessResponse()
 
 	uri := assertions.UriFromString(page.Find("span.fulluri"))
 	_, err := datastore.ActiveDataStore.FetchAssertion(uri)
@@ -127,54 +168,55 @@ func TestAddAssertion(t *testing.T) {
 }
 
 func TestSearch(t *testing.T) {
-	wt := NewWebTest(t)
+	wt := setup(t)
 	defer wt.Close()
 
-	page := wt.getPage("/")
-	page.assertHtmlQuery("#searchform", "Search for")
+	page := wt.GetPage("/")
+	page.AssertHtmlQuery("#searchform", "Search for")
 
-	page = wt.postFormData("/web/search", url.Values{"query": {"universe"}})
-	page.assertSuccessResponse()
-	page.assertHtmlQuery("h2", "Search results")
-	page.assertHtmlQuery("#content", "The universe exists")
+	page = wt.PostFormData("/web/search", url.Values{"query": {"universe"}})
+	page.AssertSuccessResponse()
+	page.AssertHtmlQuery("h2", "Search results")
+	page.AssertHtmlQuery("#content", "The universe exists")
 }
 
 func TestLoginLogout(t *testing.T) {
-	wt := NewWebTest(t)
+	wt := setup(t)
 	defer wt.Close()
 
-	page := wt.getPage("/web/login")
-	page.assertNoCookie("auth")
-	page.assertHtmlQuery("h2", "Login")
+	page := wt.GetPage("/web/login")
+	page.AssertNoCookie("auth")
+	page.AssertHtmlQuery("h2", "Login")
 
-	page = wt.postFormData("/web/login", url.Values{"user_id": {wt.user.Id}, "password": {wt.passwd}})
-	page.assertSuccessResponse()
-	page.assertHtmlQuery("span", wt.user.Id)
-	page.assertHasCookie("auth")
+	page = wt.PostFormData("/web/login", url.Values{"user_id": {wt.User.Id}, "password": {wt.Passwd}})
+	page.AssertSuccessResponse()
+	page.AssertHtmlQuery("span", wt.User.Id)
+	page.AssertHasCookie("auth")
 
-	page = wt.getPage("/web/logout")
-	page.assertHtmlQuery("#message", "You have been logged out")
-	page.assertNoCookie("auth")
+	page = wt.GetPage("/web/logout")
+	page.AssertHtmlQuery("#message", "You have been logged out")
+	page.AssertNoCookie("auth")
 }
 
 func TestBadLogin(t *testing.T) {
-	wt := NewWebTest(t)
+	wt := setup(t)
 	defer wt.Close()
 
-	page := wt.postFormData("/web/login", url.Values{"user_id": {"jkdshffkdjshdskfjhd"}, "password": {wt.passwd}})
-	page.assertHtmlQuery(".error", "Unable to verify identity")
+	page := wt.PostFormData("/web/login", url.Values{"user_id": {"jkdshffkdjshdskfjhd"}, "password": {wt.Passwd}})
+	page.AssertHtmlQuery(".error", "Unable to verify identity")
 
-	page = wt.postFormData("/web/login", url.Values{"user_id": {wt.user.Id}, "password": {"jkdfhskjfdshfk"}})
-	page.assertHtmlQuery(".error", "Unable to verify identity")
+	page = wt.PostFormData("/web/login", url.Values{"user_id": {wt.User.Id}, "password": {"jkdfhskjfdshfk"}})
+	page.AssertHtmlQuery(".error", "Unable to verify identity")
 }
 
 func TestQrCode(t *testing.T) {
-	wt := NewWebTest(t)
+	wt := setup(t)
 	defer wt.Close()
 
-	page := wt.getPage("/web/statements/e88688ef18e5c82bb8ea474eceeac8c6eb81d20ec8d903750753d3137865d10f")
-	page.assertHtmlQuery("#content", "The universe exists")
+	page := wt.GetPage("/web/statements/e88688ef18e5c82bb8ea474eceeac8c6eb81d20ec8d903750753d3137865d10f")
+	page.AssertHtmlQuery("#content", "The universe exists")
 
-	page = wt.getPage("/web/share?hash=33fe9d5eedb329c5a662d3c206d8938a33f94795c3f715be0bcd53fbdcadc7e8&type=entity")
-	page.assertHtmlQuery("h2", "Share Item")
+	page = wt.GetPage("/web/share?hash=33fe9d5eedb329c5a662d3c206d8938a33f94795c3f715be0bcd53fbdcadc7e8&type=entity")
+	page.AssertHtmlQuery("h2", "Share Item")
+	page.AssertSuccessResponse()
 }
--- internal/webtest/webtest.go
+++ internal/webtest/webtest.go
@@ -1,68 +1,27 @@
-package main
+package webtest
 
 import (
-	"crypto/rand"
-	"crypto/rsa"
-	"math/big"
 	"net/http"
-	"net/http/cookiejar"
 	"net/http/httptest"
 	"net/url"
 	"strings"
 	"testing"
 
 	"github.com/PuerkitoBio/goquery"
-	"silvatek.uk/trustedassertions/internal/assertions"
 	"silvatek.uk/trustedassertions/internal/auth"
-	"silvatek.uk/trustedassertions/internal/datastore"
 	"silvatek.uk/trustedassertions/internal/web"
 )
 
 type WebTest struct {
 	t      *testing.T
-	user   *auth.User
-	server *httptest.Server
-	passwd string
-	client *http.Client
-}
-
-func NewWebTest(t *testing.T) *WebTest {
-	wt := WebTest{t: t}
-	wt.setupTestServer()
-
-	jar, _ := cookiejar.New(nil)
-	wt.client = &http.Client{
-		Jar: jar,
-	}
-
-	return &wt
+	User   *auth.User
+	Server *httptest.Server
+	Passwd string
+	Client *http.Client
 }
 
 func (wt *WebTest) Close() {
-	wt.server.Close()
-}
-
-func (wt *WebTest) setupTestServer() {
-	initLogging()
-
-	web.TemplateDir = "../../web"
-	testDataDir = "../../testdata"
-	initDataStore()
-
-	privateKey, _ := rsa.GenerateKey(rand.Reader, 2048)
-	signer := assertions.NewEntity("Signing entity", *big.NewInt(123456))
-	signer.MakeCertificate(privateKey)
-	datastore.ActiveDataStore.Store(&signer)
-	datastore.ActiveDataStore.StoreKey(signer.Uri(), assertions.PrivateKeyToString(privateKey))
-	web.DefaultEntityUri = signer.Uri()
-
-	wt.user = &auth.User{Id: "admin"}
-	wt.passwd = "testing"
-	wt.user.HashPassword(wt.passwd)
-	wt.user.KeyRefs = append(wt.user.KeyRefs, auth.KeyRef{UserId: wt.user.Id, KeyId: signer.Uri().Unadorned(), Summary: ""})
-	datastore.ActiveDataStore.StoreUser(*wt.user)
-
-	wt.server = httptest.NewServer(setupHandlers())
+	wt.Server.Close()
 }
 
 type WebPage struct {
@@ -75,14 +34,14 @@ type WebPage struct {
 	html         *goquery.Document
 }
 
-func (wt *WebTest) getPage(path string) *WebPage {
-	url := wt.server.URL + path
+func (wt *WebTest) GetPage(path string) *WebPage {
+	url := wt.Server.URL + path
 	page := WebPage{url: url, wt: wt}
 
 	req, _ := http.NewRequest("GET", url, nil)
-	addAuthCookie(wt.user, req)
+	addAuthCookie(wt.User, req)
 
-	page.response, page.requestError = wt.client.Do(req)
+	page.response, page.requestError = wt.Client.Do(req)
 
 	if page.requestError != nil {
 		wt.t.Errorf("Error fetching %s, %v", url, page.requestError)
@@ -105,14 +64,14 @@ func addAuthCookie(user *auth.User, req *http.Request) {
 	}
 }
 
-func (wt *WebTest) postFormData(path string, data url.Values) *WebPage {
-	url := wt.server.URL + path
+func (wt *WebTest) PostFormData(path string, data url.Values) *WebPage {
+	url := wt.Server.URL + path
 	req, _ := http.NewRequest("POST", url, strings.NewReader(data.Encode()))
-	addAuthCookie(wt.user, req)
+	addAuthCookie(wt.User, req)
 
 	page := WebPage{url: url, wt: wt}
 
-	response, err := wt.client.Do(req)
+	response, err := wt.Client.Do(req)
 	if err != nil {
 		page.requestError = err
 		return &page
@@ -141,19 +100,19 @@ func (page *WebPage) Find(q string) string {
 	return page.html.Find(q).Text()
 }
 
-func (page *WebPage) assertSuccessResponse() {
+func (page *WebPage) AssertSuccessResponse() {
 	if page.statusCode >= 400 {
 		page.wt.t.Errorf("Response code indicates error: %d", page.statusCode)
 	}
 }
 
-func (page *WebPage) assertErrorResponse() {
+func (page *WebPage) AssertErrorResponse() {
 	if page.statusCode < 400 {
 		page.wt.t.Errorf("Response code does not indicate error: %d", page.statusCode)
 	}
 }
 
-func (page *WebPage) assertHtmlQuery(query string, expected string) {
+func (page *WebPage) AssertHtmlQuery(query string, expected string) {
 	if !page.ok() {
 		return
 	}
@@ -163,13 +122,13 @@ func (page *WebPage) assertHtmlQuery(query string, expected string) {
 	}
 }
 
-func (page *WebPage) assertHasCookie(name string) {
+func (page *WebPage) AssertHasCookie(name string) {
 	if !page.ok() {
 		return
 	}
-	url, _ := url.Parse(page.wt.server.URL + "/")
+	url, _ := url.Parse(page.wt.Server.URL + "/")
 
-	for _, cookie := range page.wt.client.Jar.Cookies(url) {
+	for _, cookie := range page.wt.Client.Jar.Cookies(url) {
 		if cookie.Name == name {
 			return // cookie found, no error
 		}
@@ -178,13 +137,13 @@ func (page *WebPage) assertHasCookie(name string) {
 	page.wt.t.Errorf("`%s` cookie not found", name)
 }
 
-func (page *WebPage) assertNoCookie(name string) {
+func (page *WebPage) AssertNoCookie(name string) {
 	if !page.ok() {
 		return
 	}
-	url, _ := url.Parse(page.wt.server.URL + "/")
+	url, _ := url.Parse(page.wt.Server.URL + "/")
 
-	for _, cookie := range page.wt.client.Jar.Cookies(url) {
+	for _, cookie := range page.wt.Client.Jar.Cookies(url) {
 		if cookie.Name == name {
 			page.wt.t.Errorf("`%s` cookie found", name)
 		}
