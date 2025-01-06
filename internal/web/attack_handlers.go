1b9ddc42b77e155b5985877a3cf38058f7e0f81e
Silvatek <steve@silvatek.uk>
2025-01-06 16:53:02 +0000

    More attack handler matching

--- internal/web/attack_handlers.go
+++ internal/web/attack_handlers.go
@@ -16,20 +16,24 @@ func AddAttackHandlers(r *mux.Router) {
 		"/.env", "/.env~", "/.env.dev", "/.env.local", "/.env.example", "/admin/.env", "/_all_dbs", "/login.action",
 		"/docker-compose.yml", "/user_secrets.yml", "/secrets.json",
 		"/database.sql", "/backup.sql", "/backup.zip", "/backup.tar.gz",
-		"/config.php", "/config/database.php", "/server-status", "/phpinfo.php", "/wp-config.php", "/config/database.php", "/xmlrpc.php?rsd",
+		"/index.php", "/config.php", "/config/database.php", "/server-status", "/phpinfo.php", "/wp-config.php", "/config/database.php", "/xmlrpc.php?rsd",
 	} {
 		r.HandleFunc(path, AttackHandler)
 	}
 
 	// Entire directories
 	for _, prefix := range []string{
-		"/etc/", "/.ssh/", "/.git/", "/.svn", "/_vti_pvt/", "/.vscode/", "/.kube", "/.aws", "/.docker/", "/ecp/", "/admin/", "/debug/", "/_/",
+		"/etc/", "/.ssh/", "/.git/", "/.svn", "/_vti_pvt/", "/.vscode/", "/.kube", "/.aws", "/.docker/", "/ecp/", "/admin/", "/debug/", "/_/", "/cgi-bin/",
 	} {
 		r.PathPrefix(prefix).HandlerFunc(AttackHandler)
 	}
 
 	r.MatcherFunc(func(r *http.Request, rm *mux.RouteMatch) bool {
 		return strings.Contains(r.URL.Path, "/wp-includes/")
+	}).HandlerFunc(AttackHandler)
+
+	r.MatcherFunc(func(r *http.Request, rm *mux.RouteMatch) bool {
+		return strings.Contains(r.Host, "34.160.")
 	}).HandlerFunc(AttackHandler)
 
 	r.HandleFunc("/robots.txt", RobotsTxtHandler)
@@ -43,6 +47,7 @@ func AttackHandler(w http.ResponseWriter, r *http.Request) {
 }
 
 func RobotsTxtHandler(w http.ResponseWriter, r *http.Request) {
+	SetCacheControl(w, 4*60*60)
 	w.WriteHeader(200)
 	w.Write([]byte("User-agent: *\n"))
 	w.Write([]byte("Disallow: /\n"))
