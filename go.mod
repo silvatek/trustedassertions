286cd3c5177f1ec1dfc38ee7e94d842f167317e9
Steve Leach <steve.leach@yahoo.com>
2026-08-08 13:57:27 +0100

    Passkeys investigation

--- go.mod
+++ go.mod
@@ -1,12 +1,18 @@
 module silvatek.uk/trustedassertions
 
-go 1.23.1
+go 1.25.0
 
 require (
 	cloud.google.com/go/firestore v1.17.0
 	github.com/PuerkitoBio/goquery v1.10.0
-	github.com/golang-jwt/jwt/v5 v5.2.1
+	github.com/go-webauthn/webauthn v0.16.2
+	github.com/golang-jwt/jwt/v5 v5.3.1
+	github.com/gorilla/csrf v1.7.2
+	github.com/gorilla/handlers v1.5.2
 	github.com/gorilla/mux v1.8.1
+	github.com/nbutton23/zxcvbn-go v0.0.0-20210217022336-fa2cb2858354
+	github.com/skip2/go-qrcode v0.0.0-20200617195104-da1b6568686e
+	golang.org/x/crypto v0.49.0
 	google.golang.org/api v0.196.0
 )
 
@@ -18,29 +24,32 @@ require (
 	cloud.google.com/go/longrunning v0.6.0 // indirect
 	github.com/andybalholm/cascadia v1.3.2 // indirect
 	github.com/felixge/httpsnoop v1.0.4 // indirect
+	github.com/fxamacker/cbor/v2 v2.9.1 // indirect
 	github.com/go-logr/logr v1.4.2 // indirect
 	github.com/go-logr/stdr v1.2.2 // indirect
+	github.com/go-viper/mapstructure/v2 v2.5.0 // indirect
+	github.com/go-webauthn/x v0.2.2 // indirect
 	github.com/golang/groupcache v0.0.0-20210331224755-41bb18bfe9da // indirect
+	github.com/google/go-tpm v0.9.8 // indirect
 	github.com/google/s2a-go v0.1.8 // indirect
+	github.com/google/uuid v1.6.0 // indirect
 	github.com/googleapis/enterprise-certificate-proxy v0.3.3 // indirect
 	github.com/googleapis/gax-go/v2 v2.13.0 // indirect
-	github.com/gorilla/csrf v1.7.2 // indirect
-	github.com/gorilla/handlers v1.5.2 // indirect
 	github.com/gorilla/securecookie v1.1.2 // indirect
-	github.com/nbutton23/zxcvbn-go v0.0.0-20210217022336-fa2cb2858354 // indirect
-	github.com/skip2/go-qrcode v0.0.0-20200617195104-da1b6568686e // indirect
+	github.com/philhofer/fwd v1.2.0 // indirect
+	github.com/tinylib/msgp v1.6.3 // indirect
+	github.com/x448/float16 v0.8.4 // indirect
 	go.opencensus.io v0.24.0 // indirect
 	go.opentelemetry.io/contrib/instrumentation/google.golang.org/grpc/otelgrpc v0.54.0 // indirect
 	go.opentelemetry.io/contrib/instrumentation/net/http/otelhttp v0.54.0 // indirect
 	go.opentelemetry.io/otel v1.29.0 // indirect
 	go.opentelemetry.io/otel/metric v1.29.0 // indirect
 	go.opentelemetry.io/otel/trace v1.29.0 // indirect
-	golang.org/x/crypto v0.27.0 // indirect
-	golang.org/x/net v0.29.0 // indirect
+	golang.org/x/net v0.51.0 // indirect
 	golang.org/x/oauth2 v0.22.0 // indirect
-	golang.org/x/sync v0.8.0 // indirect
-	golang.org/x/sys v0.25.0 // indirect
-	golang.org/x/text v0.18.0 // indirect
+	golang.org/x/sync v0.20.0 // indirect
+	golang.org/x/sys v0.42.0 // indirect
+	golang.org/x/text v0.35.0 // indirect
 	golang.org/x/time v0.6.0 // indirect
 	google.golang.org/genproto v0.0.0-20240903143218-8af14fe29dc1 // indirect
 	google.golang.org/genproto/googleapis/api v0.0.0-20240903143218-8af14fe29dc1 // indirect
--- internal/api/handlers.go
+++ internal/api/handlers.go
@@ -1,10 +1,13 @@
 package api
 
 import (
+	"encoding/json"
 	"net/http"
 
+	"github.com/gorilla/csrf"
 	"github.com/gorilla/mux"
 	"silvatek.uk/trustedassertions/internal/appcontext"
+	"silvatek.uk/trustedassertions/internal/auth"
 	"silvatek.uk/trustedassertions/internal/datastore"
 	log "silvatek.uk/trustedassertions/internal/logging"
 	"silvatek.uk/trustedassertions/internal/references"
@@ -14,6 +17,9 @@ func AddHandlers(r *mux.Router) {
 	r.HandleFunc("/api/v1/statements/{key}", StatementApiHandler)
 	r.HandleFunc("/api/v1/entities/{key}", EntityApiHandler)
 	r.HandleFunc("/api/v1/assertions/{key}", AssertionApiHandler)
+
+	r.HandleFunc("/api/v1/passkey/beginregistration", PassKeyRegStartHandler)
+	r.HandleFunc("/api/v1/passkey/completeregistration", PassKeyRegFinishHandler)
 
 	//r.HandleFunc("/api/v1/reindex", ReindexApiHandler)
 }
@@ -28,7 +34,7 @@ func StatementApiHandler(w http.ResponseWriter, r *http.Request) {
 		log.Errorf("Error fetching statement: %v", err)
 	}
 
-	setHeaders(w, http.StatusOK, "text/plain")
+	setHeaders(w, r, http.StatusOK, "text/plain")
 	w.Write([]byte(statement.Content()))
 }
 
@@ -37,7 +43,7 @@ func EntityApiHandler(w http.ResponseWriter, r *http.Request) {
 	key := mux.Vars(r)["key"]
 	entity, _ := datastore.ActiveDataStore.FetchEntity(ctx, references.MakeUri(key, "entity"))
 
-	setHeaders(w, http.StatusOK, "text/plain")
+	setHeaders(w, r, http.StatusOK, "text/plain")
 	w.Write([]byte(entity.Certificate))
 }
 
@@ -47,17 +53,17 @@ func AssertionApiHandler(w http.ResponseWriter, r *http.Request) {
 	assertion, err := datastore.ActiveDataStore.FetchAssertion(ctx, references.MakeUri(key, "assertion"))
 
 	if err != nil {
-		setHeaders(w, http.StatusInternalServerError, "text/plain")
+		setHeaders(w, r, http.StatusInternalServerError, "text/plain")
 		w.Write([]byte(err.Error()))
 		return
 	}
 
-	setHeaders(w, http.StatusOK, "text/plain")
+	setHeaders(w, r, http.StatusOK, "text/plain")
 	w.Write([]byte(assertion.Content()))
 }
 
 func ReindexApiHandler(w http.ResponseWriter, r *http.Request) {
-	setHeaders(w, http.StatusOK, "text/plain")
+	setHeaders(w, r, http.StatusOK, "text/plain")
 	w.Write([]byte("Reindexing..."))
 
 	datastore.ActiveDataStore.Reindex()
@@ -65,8 +71,21 @@ func ReindexApiHandler(w http.ResponseWriter, r *http.Request) {
 	w.Write([]byte("Done"))
 }
 
-func setHeaders(w http.ResponseWriter, httpStatus int, contentType string) {
-	w.WriteHeader(httpStatus)
+func setHeaders(w http.ResponseWriter, r *http.Request, httpStatus int, contentType string) {
 	w.Header().Set("Content-Type", contentType)
 	w.Header().Set("X-Robots-Tag", "noindex")
+	w.Header().Set("X-CSRF-Token", csrf.Token(r))
+	w.WriteHeader(httpStatus)
+}
+
+func PassKeyRegStartHandler(w http.ResponseWriter, r *http.Request) {
+	reg := auth.BeginPasskeyRegistration()
+	setHeaders(w, r, http.StatusOK, "application/json")
+	json.NewEncoder(w).Encode(reg.Options)
+}
+
+func PassKeyRegFinishHandler(w http.ResponseWriter, r *http.Request) {
+	auth.FinishPasskeyRegistration(r)
+	setHeaders(w, r, http.StatusOK, "application/json")
+	json.NewEncoder(w).Encode("OK")
 }
